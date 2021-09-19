80b21b39dc5fe72318b179ae8c9a82843f0e9082
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2021-09-19 13:28:55 -0300

    completed project

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Marcelo Simim on 19/09/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable{
+    let rate:Double
+}
---
+++ ByteCoin/Model/CoinManagerDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  CoinManagerDelegate.swift
+//  ByteCoin
+//
+//  Created by Marcelo Simim on 19/09/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
